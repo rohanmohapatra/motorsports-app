d48921556523976ec31307779784abbf15a9ad50
Chiranjeevi Medam <mvschiranjeevi@gmail.com>
2023-12-14 00:42:59 -0800

    F1 sub page

--- app/f1/[id].tsx
+++ app/f1/[id].tsx
@@ -1,13 +1,34 @@
-import { Box, Text } from '@gluestack-ui/themed';
+import { Box, Text, Image, View } from '@gluestack-ui/themed';
 import { useGlobalSearchParams } from 'expo-router';
 
 const f1 = () => {
     const { id } = useGlobalSearchParams();
-
+    const data = {
+        // img: merc,
+        name: 'Mercedes',
+        model: 'W13',
+        specifications: [
+            {
+                engine: 'Mercedes-AMG F1 M14 E Performance',
+                engSpec: '1.6 L V6 Turbo Charged',
+                transmission: '8-Speed Hydraulics',
+                hpower: 1070,
+                drivers: ['Lewis Hamilton', 'George Russel']
+            }
+        ]
+    };
     return (
-        <Box justifyContent="center" h="$full">
-            <Text fontFamily="Horizon">{id}</Text>
-        </Box>
+        <>
+            <Box h="$full" backgroundColor="$black">
+                <Image
+                    // style={{ alignSelf: 'flex-end' }}
+                    size="full"
+                    source={require('../../assets/f1/formula1-mercedes.jpeg')}
+                    height={300}
+                />
+                <Text fontFamily="Horizon">{id}</Text>
+            </Box>
+        </>
     );
 };
 
---
+++ app/f1/_layout.tsx
@@ -0,0 +1,17 @@
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed';
+import { useFonts } from 'expo-font';
+import { Slot, SplashScreen } from 'expo-router';
+import { useCallback } from 'react';
+import { View } from 'react-native';
+import { config } from '../../gluestack.config';
+import { Stack } from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function Layout() {
+    return (
+        <GluestackUIProvider colorMode="dark" config={config}>
+            <Slot />
+        </GluestackUIProvider>
+    );
+}
