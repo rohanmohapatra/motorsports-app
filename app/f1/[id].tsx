387e2c2a7041be9f8baf9061c8c5fbff652a167c
Chiranjeevi Medam <mvschiranjeevi@gmail.com>
2023-12-14 16:01:51 -0800

    Mercdes Icon

--- app/f1/[id].tsx
+++ app/f1/[id].tsx
@@ -2,7 +2,7 @@ import { Box, Text, Image } from '@gluestack-ui/themed';
 import { useGlobalSearchParams } from 'expo-router';
 import Logo from './F1.svg';
 import Background from './triangle.svg';
-import { ImageBackground } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 
 const f1 = () => {
     const { id } = useGlobalSearchParams();
@@ -22,26 +22,34 @@ const f1 = () => {
     };
     return (
         <>
-            <Box h="$full" backgroundColor="$black">
-                <Image
-                    size="full"
-                    source={require('../../assets/f1/formula1-mercedes.jpeg')}
-                    height={300}
-                />
-                <ImageBackground
-                    source={require('../../assets/f1/formula1-mercedes.jpeg')}
-                    style={{
-                        flex: 1,
-                        // resizeMode: 'cover',
-                        justifyContent: 'center'
-                    }}
-                >
+            <ImageBackground
+                style={{
+                    flex: 1,
+                    backgroundColor: 'black',
+                    tintColor: 'white'
+                }}
+                // tintColor="rgb(255,255,255,0.2)"
+
+                source={require('../../assets/common/triangle.png')}
+                resizeMode="cover"
+            >
+                <Box h="$full">
+                    <Image
+                        size="full"
+                        source={require('../../assets/f1/formula1-mercedes.jpeg')}
+                        height={300}
+                    />
+
                     <Box alignItems="center">
                         <Logo height="100" width={'100'} />
                         <Text fontFamily="Horizon">{id}</Text>
                     </Box>
-                </ImageBackground>
-            </Box>
+                </Box>
+            </ImageBackground>
+            {/* <View style={styles.container}>
+              
+                <View style={styles.overlay} />
+            </View> */}
         </>
     );
 };
