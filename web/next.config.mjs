37d010b11d66386f777e29e715fb97d3d7874608
rohanmohapatra <rohanironcrest14@gmail.com>
2024-02-02 00:08:04 -0800

    feat: add deploy mechanism

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'web-build'
+    /**
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+     */
+    output: 'export',
+    /**
+     * Set base path. This is the slug of your GitHub repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    basePath: '/motorsports-app',
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
